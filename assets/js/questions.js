8f41c346e62c9f724064af51e4914611f2982f1b
DrkCM <139226423+DrkCM@users.noreply.github.com>
2023-09-30 08:48:26 +0100

    Update questions.js
    Added the commits

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,22 +1,24 @@
+// this array is for the questions
+
 let questions = [
     {
-        'title': 'Which one is a UK motorcycle brand?:',
-        'choices': ['Harley Davidson', 'Triumph', 'Honda', 'Ducati'],
-        'answer': 'Triumph'
+        'title': 'Which one is a UK motorcycle brand?:', //main question
+        'choices': ['Harley Davidson', 'Triumph', 'Honda', 'Ducati'], //different choices
+        'answer': 'Triumph' //correct one!
     },
     {
-        'title': 'The capital of Spain is?:',
-        'choices': ['Madrid', 'Barcelona', 'Sevilla', 'Alicante'],
-        'answer': 'Madrid'
+        'title': 'The capital of Spain is?:', //main question
+        'choices': ['Madrid', 'Barcelona', 'Sevilla', 'Alicante'], //different choices
+        'answer': 'Madrid' //correct one!
     },
     {
-        'title': 'What movie has more Oscars:',
-        'choices': ['It', 'Sharknado', 'Titanic', 'Jaws'],
-        'answer': 'Titanic'
+        'title': 'What movie has more Oscars:', //main question
+        'choices': ['It', 'Sharknado', 'Titanic', 'Jaws'], //different choices
+        'answer': 'Titanic' //correct one!
     },
     {
-        'title': "Which one is a fruit?",
-        'choices': ['Potato', 'Onion', 'Lettuce', 'Tomato'],
-        'answer': 'Tomato'
+        'title': "Which one is a fruit?", //main question
+        'choices': ['Potato', 'Onion', 'Lettuce', 'Tomato'], //different choices
+        'answer': 'Tomato' //correct one!
     }
 ]
