b8f3d3da55e1e9a58bf62a7c0aa6b45e2a2876e7
DrkCM <139226423+DrkCM@users.noreply.github.com>
2023-09-30 08:49:11 +0100

    Update scores.js
    Added the commits

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,24 +1,29 @@
+// function for show the scores after you end game
 function printHighScores(){
-let highScores = JSON.parse(localStorage.getItem('highscores')) || [];
+let highScores = JSON.parse(localStorage.getItem('highscores')) || []; // show the score in scorespage
+
 
 highScores.sort(function(a, b) {
-    return b.score - a.score;
+    return b.score - a.score; // explain
 })
 
-highScores.forEach(function(score) {
-    let li = document.createElement('li');
-    li.textContent = `${score.initials} - ${score.score}`
+// this function is for create a score list and will be shoed when you finish the game (showing scores from higher to lower)
+highScores.forEach(function(score) { 
+    let li = document.createElement('li');  //
+    li.textContent = `${score.initials} - ${score.score}` //
 
     let ol = document.getElementById('highscores');
     ol.appendChild(li);
 })
 }
 
+//this function erase the scores frm the system
 function clearHighScores(){
    localStorage.removeItem('highscores');
    window.location.reload();
 }
 
+// define button to remove the scores
 let clearButton =  document.getElementById('clear');
 clearButton.addEventListener('click', clearHighScores);
 
